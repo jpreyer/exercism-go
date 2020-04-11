fd4b83de266f4dad0b62c965f9b8c74e708b639b
Johnny Preyer <jpreyer@rsglab.com>
2020-04-11 17:22:37 -0400

    Adding logic to have sound string return if not empty

--- raindrops/raindrops.go
+++ raindrops/raindrops.go
@@ -5,21 +5,17 @@ import "strconv"
 // Convert takes an integer as input and exports a string as detailed in the Raindrops Exercism assignment
 func Convert(a int) string {
 	var sound string
-	var divisibleBy357 bool
 
 	if a%3 == 0 {
 		sound += "Pling"
-		divisibleBy357 = true
 	}
 	if a%5 == 0 {
 		sound += "Plang"
-		divisibleBy357 = true
 	}
 	if a%7 == 0 {
 		sound += "Plong"
-		divisibleBy357 = true
 	}
-	if divisibleBy357 {
+	if sound != "" {
 		return sound
 	}
 	return strconv.Itoa(a)
