0d4db18e08aa6c15880e3480df038e2c80a967ed
Johnny Preyer <jpreyer@rsglab.com>
2020-04-11 16:19:53 -0400

    changes around first point of feedback (sound += and assigning bool a default false value

--- raindrops/raindrops.go
+++ raindrops/raindrops.go
@@ -5,17 +5,18 @@ import "strconv"
 // Convert takes an integer as input and exports a string as detailed in the Raindrops Exercism assignment
 func Convert(a int) string {
 	var sound string
-	var divisibleBy357 bool = false
+	var divisibleBy357 bool
+
 	if a%3 == 0 {
-		sound = sound + "Pling"
+		sound += "Pling"
 		divisibleBy357 = true
 	}
 	if a%5 == 0 {
-		sound = sound + "Plang"
+		sound += "Plang"
 		divisibleBy357 = true
 	}
 	if a%7 == 0 {
-		sound = sound + "Plong"
+		sound += "Plong"
 		divisibleBy357 = true
 	}
 	if divisibleBy357 {
