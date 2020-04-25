d1e63f796af5d89caac1f28c3085e7b7e67b4007
Johnny Preyer <jpreyer@rsglab.com>
2020-04-25 13:28:11 -0400

    From Exercism feedback: 1. converted map of strings to a map of runes 2. Moved map initialization to outside the function 3. Taking input into function (a string) and converting to an array of runes. 4.  Iterate over rune array with range to compute Scrabble score

--- scrabble-score/scrabble_score.go
+++ scrabble-score/scrabble_score.go
@@ -2,46 +2,46 @@ package scrabble
 
 import "strings"
 
+var values = map[rune]int{
+	'A': 1,
+	'E': 1,
+	'I': 1,
+	'O': 1,
+	'U': 1,
+	'L': 1,
+	'N': 1,
+	'R': 1,
+	'S': 1,
+	'T': 1,
+	'D': 2,
+	'G': 2,
+	'B': 3,
+	'C': 3,
+	'M': 3,
+	'P': 3,
+	'F': 4,
+	'H': 4,
+	'V': 4,
+	'W': 4,
+	'Y': 4,
+	'K': 5,
+	'J': 8,
+	'X': 8,
+	'Q': 10,
+	'Z': 10,
+}
+
 //Score takes a word as input and returns the Scrabble score for the word.
 func Score(w string) int {
 	var wordScore int
-	values := map[string]int{
-		"A": 1,
-		"E": 1,
-		"I": 1,
-		"O": 1,
-		"U": 1,
-		"L": 1,
-		"N": 1,
-		"R": 1,
-		"S": 1,
-		"T": 1,
-		"D": 2,
-		"G": 2,
-		"B": 3,
-		"C": 3,
-		"M": 3,
-		"P": 3,
-		"F": 4,
-		"H": 4,
-		"V": 4,
-		"W": 4,
-		"Y": 4,
-		"K": 5,
-		"J": 8,
-		"X": 8,
-		"Q": 10,
-		"Z": 10,
-	}
 
-	if len(w) == 0 {
-		wordScore = 0
-	} else {
-		//convert word to upper case and Iterate over letters in work
-		wordSplit := strings.Split(strings.ToUpper(w), "")
-		for i := 0; i < len(wordSplit); i++ {
-			wordScore += values[string(wordSplit[i])]
-		}
+	//convert word to upper case and Iterate over letters in work
+	w = strings.ToUpper(w)
+	//Convert w to an array of runes
+	r := []rune(w)
+	//Iterate over rune array to compile score
+	for _, v := range r {
+		wordScore += values[v]
 	}
 
 	return wordScore
