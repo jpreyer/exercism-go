72015858b3f851ff10be585779375aaf37a97b6f
Johnny Preyer <jpreyer@gmail.com>
2020-08-14 22:50:17 -0400

    changed how I handle return values, the first time I find a matching letter, return false.  Else return true at the very end.  This also means I can take away the bool variable status to track return values.

--- isogram/isogram.go
+++ isogram/isogram.go
@@ -7,7 +7,6 @@ import (
 //function will determine if input string is an isogram and return true if it is and false if not
 func IsIsogram(w string) bool {
 
-	var status bool
 	track_usage := make(map[rune]bool)
 
 	// Let's remove spaces and dashes, we don't care about multiple occurances of them
@@ -15,17 +14,17 @@ func IsIsogram(w string) bool {
 	w = strings.ReplaceAll(w, "-", "")
 	w = strings.ReplaceAll(w, " ", "")
 
-	status = true
 	// Clever way to test if a key is in a map, review!!!
 	//From https://www.golangprograms.com/how-to-check-if-a-map-contains-a-key-in-go.html
+
 	for _, v := range w {
 		if _, ok := track_usage[v]; !ok {
 			track_usage[v] = true
 		} else {
-			status = false
+			return false
 		}
 	}
 
-	return status
+	return true
 
 }
