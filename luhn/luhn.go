1f56ce3c4c67e232cb64fc5e3fa7f507652bdac9
Johnny Preyer <jpreyer@gmail.com>
2020-08-22 22:40:49 -0400

    Consolidated some code, wrote functions to sum a slice on int and another to do the calculation for the Luhn checksum.  Submitting for assessment

--- luhn/luhn.go
+++ luhn/luhn.go
@@ -5,6 +5,7 @@ import (
 	"strings"
 )
 
+//Reverse takes a string as input and returns the string in reverse order
 func Reverse(s string) string {
 	result := ""
 	for _, v := range s {
@@ -13,6 +14,34 @@ func Reverse(s string) string {
 	return result
 }
 
+//Slice_Sum takes a slice of int  as input and returns the sum of the slice
+func Slice_Sum(slice []int) int {
+	total := 0
+	for _, v := range slice {
+		total += v
+	}
+	return total
+}
+
+//Compute_Digit_Values takes a slice of int at input and returns a slice of integers that have been
+//processed by the Lutn algorithm.
+//The first step of the Luhn algorithm is to double every second digit, starting from the right.
+//If doubling the number results in a number greater than 9 then subtract 9 from the product.
+//Note: The digits we are computiung the checksum for much already be reversed!
+func Compute_Digit_Values(digits []int) []int {
+	i := 1
+	for i <= len(digits)-1 {
+		digits[i] *= 2
+		if digits[i] > 9 {
+			digits[i] -= 9
+		}
+		i += 2
+	}
+	return digits
+}
+
+//Valid takes a string as input and returns a boolean value in determining if the string is valid per the Luhn formula
+//http://en.wikipedia.org/wiki/Luhn_algorithm
 func Valid(s string) bool {
 	var rev_str []string
 	x := []int{}
@@ -33,19 +62,9 @@ func Valid(s string) bool {
 			return false
 		}
 	}
-	i := 1
-	for i <= len(x)-1 {
-		x[i] *= 2
-		if x[i] > 9 {
-			x[i] = x[i] - 9
-		}
-		i += 2
-	}
-	//Now add up x
-	sum := 0
-	for _, v := range x {
-		sum += v
-	}
+
+	x = Compute_Digit_Values(x)
+	sum := Slice_Sum(x)
 
 	if sum%10 == 0 {
 		return true
