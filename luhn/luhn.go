cfdcbaab9791c8bc5c649f09177b718ec61022a1
Johnny Preyer <jpreyer@gmail.com>
2020-09-01 21:52:30 -0400

    Feedback from Mentor 1.  No longer Reversing the input string, instead using parity of len (s) to determine to start at 0 or 1 in calculating my sum
    2.  Using unicode library to see if rune from string is a digit, no
    longer using strings.ContainAny or strings.Atoi, but more idiomatic
    go with runes.

--- luhn/luhn.go
+++ luhn/luhn.go
@@ -1,18 +1,9 @@
 package luhn
 
 import (
-	"strconv"
 	"strings"
+	"unicode"
 )
-
-//Reverse takes a string as input and returns the string in reverse order
-func Reverse(s string) string {
-	result := ""
-	for _, v := range s {
-		result = string(v) + result
-	}
-	return result
-}
 
 //Slice_Sum takes a slice of int  as input and returns the sum of the slice
 func Slice_Sum(slice []int) int {
@@ -29,7 +20,7 @@ func Slice_Sum(slice []int) int {
 //If doubling the number results in a number greater than 9 then subtract 9 from the product.
 //Note: The digits we are computiung the checksum for much already be reversed!
 func Compute_Digit_Values(digits []int) []int {
-	i := 1
+	i := len(digits) % 2
 	for i <= len(digits)-1 {
 		digits[i] *= 2
 		if digits[i] > 9 {
@@ -43,26 +34,23 @@ func Compute_Digit_Values(digits []int) []int {
 //Valid takes a string as input and returns a boolean value in determining if the string is valid per the Luhn formula
 //http://en.wikipedia.org/wiki/Luhn_algorithm
 func Valid(s string) bool {
-	var rev_str []string
+	//var rev_str []string
 	x := []int{}
-	//remove all spaces
+
 	s = strings.ReplaceAll(s, " ", "")
-	s = Reverse(s)
-	rev_str = strings.Split(s, "")
+
 	if len(s) < 2 {
 		return false
 	}
 
-	for _, v := range rev_str {
+	for _, v := range s {
 		// check to see if we have a digit [0-9]
-		if strings.ContainsAny("0123456789", string(v)) {
-			t, _ := strconv.Atoi(string(v))
-			x = append(x, t)
+		if unicode.IsDigit(v) {
+			x = append(x, int(v-'0'))
 		} else {
 			return false
 		}
 	}
-
 	x = Compute_Digit_Values(x)
 	sum := Slice_Sum(x)
 
