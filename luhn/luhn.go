d2f1c1c86cd3a086fa65da4fd58e7ef589a08c78
Johnny Preyer <jpreyer@gmail.com>
2020-09-03 22:32:21 -0400

    Last of mentor suggestions, am now using one loop and one function to do everything.

--- luhn/luhn.go
+++ luhn/luhn.go
@@ -5,50 +5,43 @@ import (
 	"unicode"
 )
 
-//ComputeDigitValues takes a slice of int at input and returns a slice of integers that have been
+//ComputeDigitValue takes a slice of int at input and returns a slice of integers that have been
 //processed by the Lutn algorithm.
 //The first step of the Luhn algorithm is to double every second digit, starting from the right.
 //If doubling the number results in a number greater than 9 then subtract 9 from the product.
 //Note: The digits we are computiung the checksum for much already be reversed!
 //We then sum up the digit values and return the sum
-func ComputeDigitValues(digits []int) int {
-	total := 0
-	i := len(digits) % 2
-	for i <= len(digits)-1 {
-		digits[i] *= 2
-		if digits[i] > 9 {
-			digits[i] -= 9
-		}
-		i += 2
+func ComputeDigitValue(digit int) int {
+	digit *= 2
+	if digit > 9 {
+		digit -= 9
 	}
-
-	for _, j := range digits {
-		total += j
-	}
-	return total
+	return digit
 }
 
 //Valid takes a string as input and returns a boolean value in determining if the string is valid per the Luhn formula
 //http://en.wikipedia.org/wiki/Luhn_algorithm
 func Valid(s string) bool {
-
-	x := []int{}
-
+	total := 0
 	s = strings.ReplaceAll(s, " ", "")
+	l := len(s)
 
-	if len(s) < 2 {
+	if l < 2 {
 		return false
 	}
 
-	for _, v := range s {
-		// check to see if we have a digit [0-9]
+	for index, v := range s {
 		if unicode.IsDigit(v) {
-			x = append(x, int(v-'0'))
+			d := int(v - '0')
+			if (l%2 == 1 && index%2 == 1) || (l%2 == 0 && index%2 == 0) {
+				total += ComputeDigitValue(d)
+			} else {
+				total += d
+			}
 		} else {
 			return false
 		}
 	}
-	sum := ComputeDigitValues(x)
-	return sum%10 == 0
 
+	return total%10 == 0
 }
