ea31c1d7bb30c7222352dd03edcb932a47d94d22
Johnny Preyer <jpreyer@gmail.com>
2020-09-05 12:26:30 -0400

    Removed function to compute didgit value and moved it to the for loop.  Also changes how SecondDigit is initialized

--- luhn/luhn.go
+++ luhn/luhn.go
@@ -5,22 +5,10 @@ import (
 	"unicode"
 )
 
-//ComputeDigitValue takes a slice of int at input and returns a slice of integers that have been
-//processed by the Lutn algorithm.
-//The first step of the Luhn algorithm is to double every second digit, starting from the right.
-//If doubling the number results in a number greater than 9 then subtract 9 from the product.
-func ComputeDigitValue(digit int) int {
-	digit *= 2
-	if digit > 9 {
-		digit -= 9
-	}
-	return digit
-}
-
 //Valid takes a string as input and returns a boolean value in determining if the string is valid per the Luhn formula
 //http://en.wikipedia.org/wiki/Luhn_algorithm
 func Valid(s string) bool {
-	var SecondDigit bool
+
 	total := 0
 
 	s = strings.ReplaceAll(s, " ", "")
@@ -30,25 +18,24 @@ func Valid(s string) bool {
 	}
 
 	//
-	//if len(s) is odd, start at 1
-	//if len(s) is even, start at zero
-	if len(s)%2 == 0 {
-		SecondDigit = true
-	} else {
-		SecondDigit = false
-	}
+	//if len(s) is odd, start at 1m so SecondDigit is false
+	//if len(s) is even, start at zero so SecondDigit is true
+	SecondDigit := len(s)%2 == 0
 
 	for _, r := range s {
 		if !unicode.IsDigit(r) {
 			return false
 		}
 
-		d := int(r - '0')
+		digit := int(r - '0')
 
 		if SecondDigit {
-			d = ComputeDigitValue(d)
+			digit *= 2
+			if digit > 9 {
+				digit -= 9
+			}
 		}
-		total += d
+		total += digit
 		SecondDigit = !SecondDigit
 	}
 
