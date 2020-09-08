e8eaaa269536a7f84131aa37fb223c0dd69e211c
Johnny Preyer <jpreyer@gmail.com>
2020-09-07 22:22:12 -0400

    First submisson for Grains

--- grains/grains.go
+++ grains/grains.go
@@ -1,1 +1,24 @@
 package grains
+
+import "errors"
+
+func Square(n int) (uint64, error) {
+	SquareTotal := uint64(1)
+	if n > 64 || n <= 0 {
+		return uint64(0), errors.New("false")
+	}
+	for i := 2; i <= n; i++ {
+		SquareTotal *= 2
+	}
+	return SquareTotal, nil
+}
+
+func Total() uint64 {
+	sum := uint64(0)
+	for i := 1; i <= 64; i++ {
+		tmp, _ := Square(i)
+		sum += tmp
+	}
+
+	return sum
+}
