3a95cf539772608f8670fbcf8709b14086c03c57
Johnny Preyer <jpreyer@gmail.com>
2020-09-12 11:23:09 -0400

    Mentor feedback, added comments to each function, also figured out how to autolint in vs code!

--- grains/grains.go
+++ grains/grains.go
@@ -2,6 +2,7 @@ package grains
 
 import "errors"
 
+//Square takes an integer n an input and outputs the number of grains on that square: 2^(n-1)
 func Square(n int) (uint64, error) {
 	SquareTotal := uint64(1)
 	if n > 64 || n <= 0 {
@@ -13,6 +14,7 @@ func Square(n int) (uint64, error) {
 	return SquareTotal, nil
 }
 
+//Total returns the total grains on a 8x8 chess board
 func Total() uint64 {
 	sum := uint64(0)
 	for i := 1; i <= 64; i++ {
