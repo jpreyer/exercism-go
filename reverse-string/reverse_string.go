2d18d0da6be49b091ce9c4e45afb5c652eb95ccf
Johnny Preyer <jpreyer@gmail.com>
2020-09-12 12:21:04 -0400

    First attempt at Reverse String exercise

--- reverse-string/reverse_string.go
+++ reverse-string/reverse_string.go
@@ -1,1 +1,16 @@
 package reverse
+
+//import "unicode/utf8"
+
+//Reverse takes a string as input and returns the strings reversed as output
+func Reverse(s string) string {
+
+	b := []rune(s)
+	RevString := make([]rune, len(b), (len(b)))
+
+	for index, r := range b {
+		RevString[len(b)-index-1] = r
+	}
+
+	return string(RevString)
+}
